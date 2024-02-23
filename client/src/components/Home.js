ef8efcb1f756d50083fc89ea3134a3dd38c53214
root <tempcbsy@gmail.com>
2024-02-23 07:48:57 +0000

    v1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Successful Amplify Deployment "Front End Amplify", "Backend Ec2 Node", "Database RDS" V1</h1>
+      <h1>Successful Amplify Deployment "Front End Amplify", "Backend Ec2 Node", "Database RDS" Version 1</h1>
     </div>
   );
 };
